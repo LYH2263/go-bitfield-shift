738f0ec75c82f632c5fabfcd4b796c6199556fd3
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-15 23:21:13 +0800

    fix: 位移越界

--- internal/bitfield/bitfield.go
+++ internal/bitfield/bitfield.go
@@ -5,6 +5,6 @@ type Bits struct{ words []uint64 }
 
 func New(n int) *Bits { return &Bits{words: make([]uint64, (n+63)/64)} }
 
-func (b *Bits) Set(bit int) { b.words[0] |= 1 << bit } // BUG
+func (b *Bits) Set(bit int) { b.words[bit/64] |= 1 << uint(bit%64) }
 
-func (b *Bits) Get(bit int) bool { return b.words[0]&(1<<bit) != 0 }
+func (b *Bits) Get(bit int) bool { return b.words[bit/64]&(1<<uint(bit%64)) != 0 }
