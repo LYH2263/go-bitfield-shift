4dfa9fba78f382d8562bd577e303e106bc9c3eb0
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-17 08:44:51 +0800

    fix: apply test model repair

--- internal/bitfield/bitfield.go
+++ internal/bitfield/bitfield.go
@@ -5,6 +5,12 @@ type Bits struct{ words []uint64 }
 
 func New(n int) *Bits { return &Bits{words: make([]uint64, (n+63)/64)} }
 
-func (b *Bits) Set(bit int) { b.words[0] |= 1 << bit } // BUG
+func (b *Bits) Set(bit int) {
+	w, off := bit/64, bit%64
+	b.words[w] |= 1 << off
+}
 
-func (b *Bits) Get(bit int) bool { return b.words[0]&(1<<bit) != 0 }
+func (b *Bits) Get(bit int) bool {
+	w, off := bit/64, bit%64
+	return b.words[w]&(1<<off) != 0
+}
